4cecbeaf8c25fde0f8b0bafa88d8548dd09ea27f
plasticchris <plasticchris@gmail.com>
2015-12-07 00:25:26 -0800

    update for pi
    fix warnings, change paths for pi, remove weird background mode, use
    lame to get mp3 & gdrive to upload

--- noise_detection.rb
+++ noise_detection.rb
@@ -29,10 +29,8 @@ HW_DETECTION_CMD = "cat /proc/asound/cards"
 # by /proc/asound/cards
 SAMPLE_DURATION = 5 # seconds
 FORMAT = 'S16_LE'   # this is the format that my USB microphone generates
-THRESHOLD = 0.05
-RECORD_FILENAME='/tmp/noise.wav'
-LOG_FILE='/var/log/noise_detector.log'
-PID_FILE='/etc/noised/noised.pid'
+RECORD_FILENAME='/home/pi/noise'
+LOG_FILE='/home/pi/noise_detector.log'
 
 logger = Logger.new(LOG_FILE)
 logger.level = Logger::DEBUG
@@ -41,17 +39,17 @@ logger.info("Noise detector started @ #{DateTime.now.strftime('%d/%m/%Y %H:%M:%S
 
 
 def self.check_required()
-  if !File.exists?('/usr/bin/arecord')
+  if !File.exist?('/usr/bin/arecord')
     warn "/usr/bin/arecord not found; install package alsa-utils"
     exit 1
   end
 
-  if !File.exists?('/usr/bin/sox')
+  if !File.exist?('/usr/bin/sox')
     warn "/usr/bin/sox not found; install package sox"
     exit 1
   end
 
-  if !File.exists?('/proc/asound/cards')
+  if !File.exist?('/proc/asound/cards')
     warn "/proc/asound/cards not found"
     exit 1
   end
@@ -72,9 +70,6 @@ optparse = OptionParser.new do |opts|
   opts.on("-n", "--threshold NOISE_THRESHOLD", "Set Activation noise Threshold. EX. 0.1") do |n|
     options[:threshold] = n
   end
-  opts.on("-e", "--email DEST_EMAIL", "Alert destination email") do |e|
-    options[:email] = e
-  end
   opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
     options[:verbose] = v
   end
@@ -88,20 +83,6 @@ optparse = OptionParser.new do |opts|
     options[:kill] = k
   end
 end.parse!
-
-if options[:kill]
-  logger.info("Terminating script");
-  logger.debug("Looking for pid file in #{PID_FILE}")
-  begin
-    pidfile = File.open(PID_FILE, "r")
-    storedpid = pidfile.read
-    Process.kill("TERM", Integer(storedpid))
-  rescue Exception => e
-    logger.error("Cannot read pid file: " + e.message)
-    exit 1
-  end
-  exit 0
-end
 
 if options[:detection]
     puts "Detecting your soundcard..."
@@ -130,7 +111,6 @@ optparse.parse!
 
 #Now raise an exception if we have not found a host option
 raise OptionParser::MissingArgument if options[:microphone].nil?
-raise OptionParser::MissingArgument if options[:email].nil?
 
 if options[:verbose]
    logger.debug("Script parameters configurations:")
@@ -139,53 +119,21 @@ if options[:verbose]
    logger.debug("Output Format: #{FORMAT}")
    logger.debug("Noise Threshold: #{THRESHOLD}")
    logger.debug("Record filename (overwritten): #{RECORD_FILENAME}")
-   logger.debug("Destination email: #{options[:email]}")
 end
 
 #Starting script part
-pid = fork do
-  stop_process = false
-  Signal.trap("USR1") do
-    logger.debug("Running...")
-  end
-  Signal.trap("TERM") do
-    logger.info("Terminating...")
-    File.delete(PID_FILE)
-    stop_process = true 
-  end
-
-  loop do
-    if (stop_process)
-	logger.info("Noise detector stopped @ #{DateTime.now.strftime('%d/%m/%Y %H:%M:%S')}")	
-	break
-    end
-    rec_out = `/usr/bin/arecord -D plughw:#{options[:microphone]},0 -d #{SAMPLE_DURATION} -f #{FORMAT} -t wav #{RECORD_FILENAME} 2>/dev/null`
-    out = `/usr/bin/sox -t .wav #{RECORD_FILENAME} -n stat 2>&1`
+loop do
+    `/usr/bin/arecord -D plughw:#{options[:microphone]},0 -d #{SAMPLE_DURATION} -f #{FORMAT} -t wav #{RECORD_FILENAME}.wav 2>/dev/null`
+    out = `/usr/bin/sox -t .wav #{RECORD_FILENAME}.wav -n stat 2>&1`
     out.match(/Maximum amplitude:\s+(.*)/m)
     amplitude = $1.to_f
-    logger.debug("Detected amplitude: #{amplitude}") if options[:verbose]
+    print("Detected amplitude: #{amplitude}") if options[:verbose]
     if amplitude > THRESHOLD
-      logger.info("Sound detected!!!")
-  
-  	# Read a file
-	filecontent = File.open(RECORD_FILENAME ,"rb") {|io| io.read}
- 	
-        encoded = [filecontent].pack("m")    # base64 econding
-puts  value = %x[/usr/sbin/sendmail #{options[:email]} << EOF
-subject: WARNING: Noise Detected
-from: home@mornati.net
-Content-Description: "noise.wav"
-Content-Type: audio/x-wav; name="noise.wav"
-Content-Transfer-Encoding:base64
-Content-Disposition: attachment; filename="noise.wav"
-#{encoded}
-EOF] 
+        print ("Sound detected!!!")
+
+	`lame #{RECORD_FILENAME}.wav #{RECORD_FILENAME}.mp3`
+	`drive upload --file #{RECORD_FILENAME}.mp3`
     else
       logger.debug("No sound detected...")
     end
 end
-end
-
-Process.detach(pid)
-logger.debug("Started... (#{pid})")
-File.open(PID_FILE, "w") { |file| file.write(pid) }
